df949f54a7aece98a938cbb9428b4d0a8be5753e
MiguelPort80 <163421687+MiguelPort80@users.noreply.github.com>
2024-09-27 14:57:08 -0300

    Atualizado database.php

--- app/functions/database.php
+++ app/functions/database.php
@@ -1,7 +1,7 @@
 <?php
 function connect()
 {
-  $pdo = new \PDO("mysql:host=localhost;dbname=banco_teste;charset=utf8", 'teste', 'password');
+  $pdo = new \PDO("mysql:host=localhost;dbname=banco_teste;charset=utf8", 'user', 'password');
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
   return $pdo;
